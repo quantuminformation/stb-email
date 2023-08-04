6a5470b9f0bb873c2cda5b0b4ab060a81275f46d
Nikos <nkatsikanis@gmail.com>
2023-08-04 14:56:32 +0300

    table rows

--- node/email.mjs
+++ node/email.mjs
@@ -1,12 +1,13 @@
-// node/email.mjs
-import {imageRoot} from './config.mjs'
+import {imageRoot} from './config.mjs';
 import {config} from 'dotenv';
+import nodemailer from 'nodemailer';
 
 config();
 
-import nodemailer from 'nodemailer';
-
-const bodyText = 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath';
+const bodyText = 'Hello Nikos ' +
+    '' +
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. ' +
+    'Nullam quis ante. Etiam sit amet orci eget eros faucib'; // Truncated for brevity
 
 const tableColumnData = [
     {
@@ -15,6 +16,7 @@ const tableColumnData = [
         location: 'Location 1',
         name: 'Name 1',
         description: 'Description 1',
+        link: 'https://www.google.com'
     },
     {
         title: 'Title 2',
@@ -22,9 +24,41 @@ const tableColumnData = [
         location: 'Location 2',
         name: 'Name 2',
         description: 'Description 2',
+        link: 'https://www.google.com'
+    }
+];
 
-    }]
+let tableHtmlRows = tableColumnData.map(data => `
+    <td>📆 ${data.dateRange}<br>
+        📍 ${data.location}<br>
+       👩 ${data.name}<br>
+        ${data.description}
+    </td>`).join('');
 
+let tableHtmlTitles = tableColumnData.map(data => `<td>${data.title}</td>`).join('');
+
+let tableHtml = `
+  <table style="width: 100%; color: #ffffff; margin: auto; border:  1px solid black">
+    <tr>${tableHtmlTitles}</tr>
+    <tr>${tableHtmlRows}</tr>
+  </table>
+`;
+
+let emailHtmlContent = `
+  <div style="background-color: #313132; color: #ffffff; padding:1rem">
+    <img src="${imageRoot}/STB-Logo-neu_transparent.png" style="width: 12rem; display: block;" alt="Logo"/>
+    <img src="${imageRoot}/hero.png" style="width: 100%;" alt="Hero image"/>
+    <p style="text-align: center;">${bodyText}</p>
+    ${tableHtml}
+    <p style="text-align: center;">Footer content here</p>
+  </div>
+`;
+
+let emailPlainTextContent = `
+  ${bodyText}
+  ${tableColumnData}
+`;
+console.log(emailPlainTextContent);
 async function main() {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -37,36 +71,7 @@ async function main() {
         },
     });
 
-    let emailHtmlContent = `
-  <div style="background-color: #313132; color: #ffffff;">
-    <img src="${imageRoot}/STB-Logo-neu_transparent.png" width="12rem" alt="Logo"/>
-    <img src="${imageRoot}/hero.png" width="100rem" alt="Hero image"/>
-    <p style="text-align: center;">${bodyText}</p>
-    <table style="width: 50%; color: #ffffff; margin: auto;">
-      <tr>
-        <td>Title 1</td>
-        <td>Details 1</td>
-        <td><a href="#" style="color: #ffffff;">Button 1</a></td>
-      </tr>
-      <tr>
-        <td>Title 2</td>
-        <td>Details 2</td>
-        <td><a href="#" style="color: #ffffff;">Button 2</a></td>
-      </tr>
-      <tr>
-        <td>Title 3</td>
-        <td>Details 3</td>
-        <td><a href="#" style="color: #ffffff;">Button 3</a></td>
-      </tr>
-    </table>
-    <p style="text-align: center;">Footer content here</p>
-  </div>`;
-
-    let emailPlainTextContent = `
-  ${bodyText}
-`;
-
-// send mail with defined transport object
+    // send mail with defined transport object
     let info = await transporter.sendMail({
         from: process.env.YOUR_EMAIL, // sender address
         to: process.env.RECIPIENT_EMAIL, // list of receivers
@@ -76,7 +81,6 @@ async function main() {
     });
 
     console.log("Message sent: %s", info.messageId);
-
 }
 
 main().catch(console.error);
