12d2a689c5b8d2b4357ef32f9ec2616eb14d8917
Nikos <nkatsikanis@gmail.com>
2023-08-19 18:10:07 +0200

    template salto wip

--- node/templates/Salto1848.mjs
+++ node/templates/Salto1848.mjs
@@ -1,13 +1,14 @@
 import { imageRoot } from '../config.mjs';
 
-const isWhiteText = true; // You can change this value to false for black text
-const textColor = isWhiteText ? '#ffffff' : '#000000';
+//const isWhiteText = true; // You can change this value to false for black text
+//const textColor = isWhiteText ? '#ffffff' : '#000000';
+const textColor = '#777777';
 const linkCSS = 'color: #e74c3c; font-weight: bold; text-decoration: none;';
 
 const link =
   'https://mailing.stb.de/link.php?link=01_02_04_784_6_04-3CE-01-9FB063983E6E8D12929A925F38A20454-FFC1F06C0B7FA6A3E49D&utm_source=mailing.stb.de&utm_medium=mailing&utm_campaign=allgemein-theo';
 
-const bodyText = ` <div style='text-align:  center;'><p>Hallo liebe Vereinsvertretende,<br><br>
+const bodyHTML = ` <div style='text-align:  center;'><p>Hallo liebe Vereinsvertretende,<br><br>
 
 ab heute ist die neue Ausgabe des <strong>Salto 1848</strong> wieder online verfügbar. 
 Klickt euch direkt rein, erfahrt viele spannende Dinge aus der Vereinswelt und schaut euch die neuen Praxistipps im Kinderturnen, Turnen sowie in der <strong>GYMWELT</strong> an.
@@ -32,22 +33,24 @@ const tableColumnData = [
 let tableHtmlRows = tableColumnData
   .map(
     (data) => `
-    <td style='border: 2px solid black; width: 50%; text-align: center; padding: 1rem;'>${data.content}</td>`,
+    <td style='width: 50%; text-align: center; padding: 1rem;'>${data.content}</td>`,
   )
   .join('');
 
 let tableHtml = `
-  <table style='color: #ffffff; width: 100%; border-collapse: collapse;'>
+  <table style='color: #ffffff; width: 100%; border-collapse: collapse; padding-bottom: 1rem'>
     <tr>${tableHtmlRows}</tr>
   </table>
 `;
 
 let emailHtmlContent = `
-  <div style='background-color: #313132; color: ${textColor}; font-size: 1.1rem; font-family:Calibri;'>
+  <div style='background-color: #313132; color: ${textColor}; font-size: 1.1rem; font-family:sans-serif;'>
+  <div style='background-color: white'>
     <img src='${imageRoot}logo.png'  style='width: 10rem;  padding:1rem; display: block;' alt='Logo'/>
+    </div>
     <img src='${imageRoot}hero_salto.jpeg' style='width: 100%;'  alt='Hero image'/>
     <div style=' padding:1rem; max-width: 50rem; width:50rem'>
-     ${bodyText}
+     ${bodyHTML}
       ${tableHtml}
       
   
@@ -55,6 +58,7 @@ let emailHtmlContent = `
 Fritz-Walter-Weg 19<br>
 70372 Stuttgart<br>
 Tel: 0711 / 49092 - 0</span></div>
+<!--for large links please use variables, don't put in html-->
 
 <!--
 <span style='font-size:11pt'><span ><a href='mailto:info@stb.de' target='_blank'><span style='color:#000000'>info@stb.de</span></a></span></span></div>
@@ -72,6 +76,10 @@ Tel: 0711 / 49092 - 0</span></div>
  
 `;
 
-let emailPlainTextContent = bodyText;
+let emailPlainTextContent = bodyHTML;
 
 export { emailHtmlContent, emailPlainTextContent };
+
+//  rgb(119,119,199) to hash code
+
+//  #7777c7
